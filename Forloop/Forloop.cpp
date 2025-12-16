21c3164c6616a845a474791da5beba7d4413ec69
Phatchara6711800003 <6711800003@mut.ac.th>
2025-12-16 23:17:23 +0700

    LAB05

---
+++ Forloop/Forloop.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void processNumber(int input, int& minValue, int& count)
+{
+    if (count == 0 || input < minValue) {
+        minValue = input;
+    }
+    count++;
+}
+int main()
+{
+    int number;
+    int minValue = 0;
+    int count = 0;
+    while (true) {
+        cout << "Enter number: ";
+        cin >> number;
+        if (number == 0) {
+            break;
+        }
+        processNumber(number, minValue, count);
+    }
+    cout << endl;
+    cout << "Total numbers entered: " << count << endl;
+    if (count > 0) {
+        cout << "Minimum value: " << minValue << endl;
+    }
+    else {
+        cout << "No numbers were entered." << endl;
+    }
+    return 0;
+}
