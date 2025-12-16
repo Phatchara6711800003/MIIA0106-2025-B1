53f651417acd303d9f07350b6c40d0d8f99b90d8
Phatchara6711800003 <6711800003@mut.ac.th>
2025-12-16 16:59:35 +0700

    LAB5

---
+++ LAB5/LAB5 No1/LAB5 No1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+double calculateArea(double x)
+{
+    double PI = 3.14159;
+    double area = PI * x * x;
+    return area;
+
+}
+
+double calculateRectangle(double width, double height)
+{
+    return width * height;
+}
+
+int main()
+{
+    const double PI = 3.14159;
+    double radius;
+    cout << "Enter the radius of the circle: ";
+    cin >> radius;
+
+    double area = calculateArea(radius);
+    cout << "The area of the circle with radius: " << radius << " is " << area << endl;
+
+
+    double width, height;
+    cout << "Enter  width  of  rectangle: ";
+    cin >> width;
+    cout << "Enter  height  of  rectangle: ";
+    cin >> height;
+
+    double rectangleArea = calculateRectangle(width, height);
+    cout << "The area of the rectangle with width: " << width << " and height: " << height << " is " << rectangleArea << endl;
+    return 0;
+}
---
+++ LAB5/LAB5 No3/LAB5 No3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+// ฟังก์ชัน swap แบบ Call by Value
+void swapByValue(int a, int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+// ฟังก์ชัน swap แบบ Call by Reference
+void swapByReference(int& a, int& b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    int a, b;
+    cout << "Enter value for a: ";
+    cin >> a;
+    cout << "Enter value for b: ";
+    cin >> b;
+    cout << "Before swap: a = " << a << ", b = " << b << endl;
+
+    swapByValue(a, b);
+    cout << "After swap (Call by Value): a = " << a << ", b = " << b << endl;
+    swapByReference(a, b);
+    cout << "After swap (Call by Reference): a = " << a << ", b = " << b << endl;
+    return 0;
+}
